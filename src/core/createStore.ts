6307012f0c521d4dd282117112ea5c38f4baf052
saber2pr <saber2pr@gmail.com>
2019-03-23 22:33:19 +0000

    修复action泛型丢失

--- src/core/createStore.ts
+++ src/core/createStore.ts
@@ -2,7 +2,7 @@
  * @Author: saber2pr
  * @Date: 2019-03-23 21:17:25
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-03-23 21:56:07
+ * @Last Modified time: 2019-03-23 22:31:25
  */
 import React, { Reducer } from 'react'
 
@@ -10,7 +10,7 @@ export type getState<S> = () => S
 
 export type dispatch<A> = (action: A) => void
 
-export type useReducer<S, A> = (
+export type useReducer<S> = <A>(
   reducer: Reducer<S, A>,
   initialState: S
 ) => [S, dispatch<A>]
@@ -20,17 +20,16 @@ export type useReducer<S, A> = (
  *
  * @export
  * @template S
- * @template A
- * @param {S | undefined} initialState
- * @returns {[getState<S>, useReducer<S, A>]} [getState, useReducer]
+ * @param {S} initialState
+ * @returns {[getState<S>, useReducer<S[keyof S]>]} [getState, useReducer]
  */
-export function createStore<S, A>(
-  initialState?: S | undefined
-): [getState<S>, useReducer<S[keyof S], A>] {
+export function createStore<S>(
+  initialState: S
+): [getState<S>, useReducer<S[keyof S]>] {
   const __STORE: S = initialState
   const getState: getState<S> = () => __STORE
 
-  const useReducer: useReducer<S[keyof S], A> = (reducer, initialState) => {
+  const useReducer: useReducer<S[keyof S]> = (reducer, initialState) => {
     const [state, setState] = React.useState(initialState)
     return [
       state,
